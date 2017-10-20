003a71370caa4412372f12015b72f292eddd9701
colepacak <cole@favish.com>
2017-10-20 09:10:04 -0500

    pare down react scripts boilerplate, enable socket.io on client establish connection with service

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
 
 class App extends Component {
+
+  componentWillMount() {
+    this.socket = io('http://localhost:4000');
+  }
+
+  _handleClick() {
+    this.socket.emit('chat message', 'ahoy');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this._handleClick.bind(this)}>Send chat message</button>
       </div>
     );
   }
--- service/index.js
+++ service/index.js
@@ -1,13 +1,12 @@
 import http from 'http';
 import socket_io from 'socket.io';
 
-const server = http.createServer();
 const port = 4000;
-let io = socket_io(server);
-
-server.on('request', (req, res) => {
-  res.end('<html><body><h1>Hello, World!</h1></body></html>');
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.end();
 });
+let io = socket_io(server);
 
 server.listen(port, () => {
   console.log('listening on ' + port)
@@ -15,14 +14,13 @@ server.listen(port, () => {
 
 io.on('connection', socket => {
   console.log('a user connected');
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
-    socket.broadcast.emit('broadcast');
-  });
 
-  socket.on('message', msg => {
-    socket.broadcast.emit('message', msg);
+  socket.on('chat message', function(msg){
+    console.log('chat message: ' + msg);
+    socket.broadcast.emit('broadcast', msg);
   });
 });
